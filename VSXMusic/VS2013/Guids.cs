313575faeb42bcaf44c4531e10699c5e4225bc06
NanQi <luyu_1216@126.com>
2015-01-17 11:21:03 +0800

    add vs2013 package;

---
+++ VSXMusic/VS2013/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Ancientomb.VS2013
+{
+    static class GuidList
+    {
+        public const string guidVS2013PkgString = "3a5a6347-784b-42bb-a0a2-5554eee04673";
+        public const string guidVS2013CmdSetString = "cf05f13c-90c8-4bc9-b566-5ddeb7833763";
+
+        public static readonly Guid guidVS2013CmdSet = new Guid(guidVS2013CmdSetString);
+    };
+}
